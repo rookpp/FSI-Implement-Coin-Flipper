516f89bc32b3fb93921ee17a3afdec2ad5b9d9dd
Kamil Holub <kahol20000@gmail.com>
2023-03-30 00:42:23 -0400

    Finished activity

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,42 @@
-// TODO: Declare any global variables we need
-
+let heads = 0;
+let tails = 0;
 
 document.addEventListener('DOMContentLoaded', function () {
-    // This is just a sanity check to make sure your JavaScript script is getting loaded
-    // You can remove it once you see it in your browser console in the developer tools
-    console.log('Hi')
-
-    // TODO: Add event listener and handler for flip and clear buttons
-
-    // Flip Button Click Handler
-        // TODO: Determine flip outcome
-        // TODO: Update image and status message in the DOM
-
-        // Update the scorboard
-        // TODO: Calculate the total number of rolls/flips
-        // Make variables to track the percentages of heads and tails
-        // TODO: Use the calculated total to calculate the percentages
-        // HINT: Make sure not to divide by 0! (if total is 0, percent will be 0 as well)
-        // TODO: Update the display of each table cell
-
-
-    // Clear Button Click Handler
-        // TODO: Reset global variables to 0
-        // TODO: Update the scoreboard (same logic as in flip button click handler)
-
+    document.getElementById('flip').addEventListener('click', function ()  {
+            let flip = Math.random() <.5
+        if(flip){     //values of 1 are = to true, 0 = to false
+            heads+=1
+            document.getElementById('penny-image').src = 'assets\\images\\penny-heads.jpg';
+            document.getElementById('message').textContent = 'You Flipped Heads!'
+        }
+        else{
+            tails+=1
+            document.getElementById('penny-image').src = 'assets\\images\\penny-tails.jpg';
+            document.getElementById('message').textContent = 'You Flipped Tails!'
+        }
+        let sum = heads+tails;
+    let pheads = 0;
+    let ptails = 0;
+    if (sum !=0)
+    {
+        pheads = (heads/sum)*100;
+        ptails = (tails/sum)*100;
+    }
+    document.getElementById('heads').textContent = heads;
+    document.getElementById('heads-percent').textContent = pheads.toFixed(2) + '%';
+    document.getElementById('tails').textContent = tails;
+    document.getElementById('tails-percent').textContent = ptails.toFixed(2) + '%';
+    })
+    document.getElementById('clear').addEventListener('click',function(){
+        heads = 0;
+        tails=0;
+        pheads=0;
+        ptails=0;
+        document.getElementById('message').textContent = 'Let\'s Get Rolling!'
+        document.getElementById('heads').textContent = heads;
+        document.getElementById('heads-percent').textContent = pheads.toFixed(2) + '%';
+        document.getElementById('tails').textContent = tails;
+        document.getElementById('tails-percent').textContent = ptails.toFixed(2) + '%';
+        document.getElementById('penny-image').src = 'assets\\images\\penny-heads.jpg';
+    })
 })
